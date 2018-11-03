a2364b4f6a7dff9c3cb8e68b0d3bd3b30cca6cf5
wulin <15123552246@163.com>
2018-11-03 23:46:44 +0800

    代码提交
    Signed-off-by: wulin <15123552246@163.com>

---
+++ src/main/java/com/testingedu/antoy/App.java
@@ -0,0 +1,13 @@
+package com.testingedu.antoy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
