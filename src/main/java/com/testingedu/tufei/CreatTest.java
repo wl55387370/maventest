562f5f3b6793b07fcdfaca0320c1ec7e06e30ed9
wulin <15123552246@163.com>
2018-11-25 18:58:29 +0800

    完成购物模块
    Signed-off-by: wulin <15123552246@163.com>

--- src/main/java/com/testingedu/tufei/CreatTest.java
+++ src/main/java/com/testingedu/tufei/CreatTest.java
@@ -13,13 +13,17 @@ public class CreatTest {
 	//执行Login模块自动化
 	
     public void creat() throws InterruptedException {
-    	//打开页面
+    	//打开购物页面
     	driver.get("http://www.testingedu.com.cn:8000/");
-    
+    //搜索手机
     	driver.findElement(By.xpath("//*[@id=\"q\"]")).sendKeys("手机");
+    	//点击搜索
     	driver.findElement(By.xpath("//*[@id=\"searchForm\"]/button")).click();
+    	//选择第一个商品
     	driver.findElement(By.xpath("/html/body/div[4]/div/div[2]/div[2]/ul/li[1]/div/div[1]/a/img")).click();
+    	//添加购物车
     	driver.findElement(By.xpath("//*[@id=\"join_cart\"]")).click();
+    	
     	
     	System.out.println("加入购物车");
     }
--- src/main/java/com/testingedu/will/LoginTest.java
+++ src/main/java/com/testingedu/will/LoginTest.java
@@ -15,12 +15,14 @@ public class LoginTest {
     public void login() throws InterruptedException {
     	//打开登录页面
     	driver.get("http://www.testingedu.com.cn:8000/Home/user/login.html");
-    	//进入iframe标签
+    	//输入手机号
     
     	driver.findElement(By.xpath("//*[@id=\"username\"]")).sendKeys("13800138006");
-    	
+    	//输入密码
     	driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("123456");
+    	//输入验证码
     	driver.findElement(By.xpath("//*[@id=\"verify_code\"]")).sendKeys("1111");
+    	//点击登录
     	
     	driver.findElement(By.xpath("//*[@id=\"loginform\"]/div/div[6]/a")).click();
     	
